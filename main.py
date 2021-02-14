2fd13a5a8180f2478fab5007672b2e8b13af1e72
Álvaro Camacho <mauricio-xxi@hotmail.com>
2021-02-14 23:36:40 +0100

    plot the results

--- main.py
+++ main.py
@@ -73,3 +73,12 @@ x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 predicted_prices = model.predict(x_test)
 predicted_prices = scaler.inverse_transform(predicted_prices)
 
+# Plot the test prediction
+
+plt.plot(actual_prices, color="black", label=f"Actual{company}price")
+plt.plot(predicted_prices,  color="green", label=f"Predicted{company} price")
+plt.title((f"{company} Share price"))
+plt.xlabel("Time")
+plt.ylabel(f"{company} Share price")
+plt.legend()
+plt.show()
