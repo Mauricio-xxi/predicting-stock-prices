4ec220a4dbfeea0ab7064c71f63bf95e0dcacca1
Álvaro Camacho <mauricio-xxi@hotmail.com>
2021-02-14 23:35:59 +0100

    test model, load test data and prepare test data

--- main.py
+++ main.py
@@ -45,3 +45,18 @@ model.add(Dense(units=1))   # prediction the next closing price
 model.compile(optimizer="adam", loss="mean_squared_error")
 model.fit(x_train, y_train, epochs=25, batch_size=32)
 
+""" Test the model accuracy on Existing data"""
+
+test_start = dt.datetime(2020, 1, 1)
+test_end = dt.datetime.now()
+
+test_data = web.DataReader(company, "yahoo", test_start, test_end)
+actual_prices = test_data["Close"].values
+
+total_dataset = pd.concat((data["Close"], test_data["Close"]), axis=0)
+
+model_inputs = total_dataset[len(
+    total_dataset)-len(test_data)-prediction_days:].values
+model_inputs = model_inputs.reshape(-1, 1)
+model_inputs = scaler.transform(model_inputs)
+
